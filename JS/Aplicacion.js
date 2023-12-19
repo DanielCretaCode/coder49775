3f15582fe86440179638153c4685f8747bae48dd
DanielCretaCode <149549874+DanielCretaCode@users.noreply.github.com>
2023-12-18 21:25:01 -0300

    Aplicacion.js

--- JS/Aplicacion.js
+++ JS/Aplicacion.js
@@ -1,89 +1,177 @@
-function Ticket(number, description) {
-  this.number = number;
-  this.description = description;
-}
+const ticketManager = {
+  tickets: [],
 
-const ticketList = document.getElementById('ticketList');
-const loginButton = document.getElementById('loginButton');
-const logoutButton = document.getElementById('logoutButton');
-const generateTicketButton = document.getElementById('generateTicketButton');
-// Usuario para testear
-const userEmail = 'coder@house.com';
-const userPassword = '12345';
-let ticketCounter = 1;
+  init() {
+    this.setupEventListeners();
+    this.checkUserSession();
+    this.loadTickets();
+  },
 
-if (!isUserLoggedIn()) {
-  
-  loginButton.disabled = false;
-  logoutButton.disabled = true;
-  generateTicketButton.disabled = true;
-} else {
-  
-  loginButton.disabled = true;
-  logoutButton.disabled = false;
-  generateTicketButton.disabled = false;
+  setupEventListeners() {
+    const loginButton = document.getElementById('loginButton');
+    const logoutButton = document.getElementById('logoutButton');
+    const generateTicketButton = document.getElementById('generateTicketButton');
 
-  
-  const tickets = JSON.parse(localStorage.getItem('tickets'));
-  if (tickets) {
-    tickets.forEach(ticket => {
+    loginButton.addEventListener('click', () => this.handleLogin());
+    logoutButton.addEventListener('click', () => this.handleLogout());
+    generateTicketButton.addEventListener('click', () => this.handleGenerateTicket());
+
+    const closeTicketButtons = document.getElementsByClassName('closeTicket');
+    for (const button of closeTicketButtons) {
+      button.addEventListener('click', (event) => {
+        const ticketIndex = event.target.dataset.index;
+        this.closeTicket(ticketIndex);
+      });
+    }
+
+    const deleteTicketButtons = document.getElementsByClassName('deleteTicket');
+    for (const button of deleteTicketButtons) {
+      button.addEventListener('click', (event) => {
+        const ticketIndex = event.target.dataset.index;
+        this.deleteTicket(ticketIndex);
+      });
+    }
+  },
+
+  checkUserSession() {
+    const loginButton = document.getElementById('loginButton');
+    const logoutButton = document.getElementById('logoutButton');
+    const generateTicketButton = document.getElementById('generateTicketButton');
+
+    const userEmail = localStorage.getItem('userEmail');
+    const userPassword = localStorage.getItem('userPassword');
+
+    if (!userEmail || !userPassword) {
+      loginButton.disabled = false;
+      logoutButton.disabled = true;
+      generateTicketButton.disabled = true;
+    } else {
+      loginButton.disabled = true;
+      logoutButton.disabled = false;
+      generateTicketButton.disabled = false;
+    }
+  },
+
+  renderTickets() {
+    const ticketList = document.getElementById('ticketList');
+    ticketList.innerHTML = '';
+
+    this.tickets.forEach((ticket, index) => {
       const ticketItem = document.createElement('div');
-      ticketItem.textContent = `Ticket #${ticket.number}: ${ticket.description}`;
+      ticketItem.classList.add('ticket');
+
+      const ticketInfo = document.createElement('div');
+      ticketInfo.classList.add('ticket-info');
+
+      const number = document.createElement('p');
+      number.textContent = `Ticket #${ticket.number}`;
+
+      const description = document.createElement('p');
+      description.textContent = `Descripción: ${ticket.description}`;
+
+      const date = document.createElement('p');
+      date.textContent = `Fecha de creación: ${ticket.date}`;
+
+      const status = document.createElement('p');
+      status.textContent = `Estado: ${ticket.status}`;
+
+      const closeButton = document.createElement('button');
+      closeButton.textContent = 'Cerrar';
+      closeButton.dataset.index = index;
+      closeButton.classList.add('closeTicket');
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Eliminar';
+      deleteButton.dataset.index = index;
+      deleteButton.classList.add('deleteTicket');
+
+      ticketInfo.appendChild(number);
+      ticketInfo.appendChild(description);
+      ticketInfo.appendChild(date);
+      ticketInfo.appendChild(status);
+      ticketInfo.appendChild(closeButton);
+      ticketInfo.appendChild(deleteButton);
+
+      ticketItem.appendChild(ticketInfo);
       ticketList.appendChild(ticketItem);
     });
-  }
-}
 
-function isUserLoggedIn() {
-  const userEmail = localStorage.getItem('userEmail');
-  const userPassword = localStorage.getItem('userPassword');
+    this.setupEventListeners(); // Reconfiguramos los event listeners después de renderizar los tickets
+  },
 
-  return userEmail && userPassword;
-}
+  handleLogin() {
+    const email = prompt('Ingrese su correo electrónico:') || '';
+    const password = prompt('Ingrese su contraseña:') || '';
 
-loginButton.addEventListener('click', () => {
-  const email = prompt('Ingrese su correo electrónico:') || '';
-  const password = prompt('Ingrese su contraseña:') || '';
+    console.log('Correo ingresado:', email);
+    console.log('Contraseña ingresada:', password);
 
-  if (email === userEmail && password === userPassword) {
-    localStorage.setItem('userEmail', email);
-    localStorage.setItem('userPassword', password);
+    const userEmail = 'coder@house.com';
+    const userPassword = '12345';
 
-    loginButton.disabled = true;
-    logoutButton.disabled = false;
-    generateTicketButton.disabled = false;
-  } else {
-    alert('Las credenciales no son correctas.');
-  }
-});
+    console.log('Correo esperado:', userEmail);
+    console.log('Contraseña esperada:', userPassword);
 
-logoutButton.addEventListener('click', () => {
-  localStorage.removeItem('userEmail');
-  localStorage.removeItem('userPassword');
+    if (email === userEmail && password === userPassword) {
+      localStorage.setItem('userEmail', email);
+      localStorage.setItem('userPassword', password);
+      this.checkUserSession();
+      this.renderTickets();
+    } else {
+      alert('Las credenciales no son correctas.');
+    }
+  },
 
-  loginButton.disabled = false;
-  logoutButton.disabled = true;
-  generateTicketButton.disabled = true;
-});
+  handleLogout() {
+    localStorage.removeItem('userEmail');
+    localStorage.removeItem('userPassword');
+    this.checkUserSession();
+    this.renderTickets();
+  },
 
-generateTicketButton.addEventListener('click', () => {
-  const description = prompt('Ingrese la descripción para el nuevo ticket:') || '';
+  handleGenerateTicket() {
+    const description = prompt('Ingrese la descripción para el nuevo ticket:') || '';
+    const currentDate = new Date().toLocaleString();
+    const status = 'Abierto';
 
-  if (description === '') {
-    alert('La descripción no puede estar vacía.');
-    return;
-  }
+    const newTicket = {
+      number: this.tickets.length + 1,
+      description,
+      date: currentDate,
+      status,
+    };
 
-  const newTicket = new Ticket(ticketCounter, description);
-  const tickets = JSON.parse(localStorage.getItem('tickets')) || [];
-  tickets.push(newTicket);
-  localStorage.setItem('tickets', JSON.stringify(tickets));
+    this.tickets.push(newTicket);
+    localStorage.setItem('tickets', JSON.stringify(this.tickets));
 
-  const ticketItem = document.createElement('div');
-  ticketItem.textContent = `Ticket #${newTicket.number}: ${newTicket.description}`;
-  ticketList.appendChild(ticketItem);
+    this.renderTickets();
+  },
 
-  ticketCounter++;
+  loadTickets() {
+    fetch('./JS/tickets.json')
+      .then(response => response.json())
+      .then(data => {
+        this.tickets = data;
+        this.renderTickets();
+      })
+      .catch(error => {
+        console.error('Error fetching tickets:', error);
+      });
+  },
 
-  console.log(`Ticket #${newTicket.number} generado con la descripción: ${newTicket.description}`);
-});
+  closeTicket(index) {
+    if (index >= 0 && index < this.tickets.length) {
+      this.tickets[index].status = 'Cerrado';
+      this.renderTickets();
+    }
+  },
+
+  deleteTicket(index) {
+    if (index >= 0 && index < this.tickets.length) {
+      this.tickets.splice(index, 1);
+      this.renderTickets();
+    }
+  },
+};
+
+ticketManager.init();
